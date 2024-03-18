d1d3c320a4abc5deae734c42662092364e8542e7
Andreas Mautsch <amautsch>
2024-03-18 11:48:09 +0100

    changes

--- src/main/java/org/goafabric/invoice/extensions/HttpInterceptor.java
+++ src/main/java/org/goafabric/invoice/extensions/HttpInterceptor.java
@@ -1,7 +1,7 @@
 package org.goafabric.invoice.extensions;
 
 public class HttpInterceptor {
-    private static final String JWT_USER1 = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJpYXQiOjE1MTYyMzkwMjJ9.-JxynqYw5AhNqgwJV8yeKSCOOfYF0oMsO2arF2b4a5E";
+    private static final String JWT_USER1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicHJlZmVycmVkX3VzZXJuYW1lIjoidXNlcjEiLCJpYXQiOjE1MTYyMzkwMjJ9.-JxynqYw5AhNqgwJV8yeKSCOOfYF0oMsO2arF2b4a5E";
 
     public static String getToken() {
         return JWT_USER1;
