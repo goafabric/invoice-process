a0f0fe23b01d223853c367e4394a71a0c524116f
Andreas Mautsch <amautsch>
2024-03-20 08:36:28 +0100

    controller added

--- src/main/java/org/goafabric/invoice/process/ProcessLauncher.java
+++ src/main/java/org/goafabric/invoice/process/ProcessLauncher.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping(value = "processes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProcessLauncher implements CommandLineRunner {
     private @Value("${test.mode:false}") Boolean testMode;
     private @Value("${process.autostart:false}") Boolean processAutoStart;
@@ -15,6 +19,11 @@ public class ProcessLauncher implements CommandLineRunner {
 
     public ProcessLauncher(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    @GetMapping("start")
+    public void start() {
+        applicationContext.getBean(InvoiceProcess.class).run();
     }
 
     @Override
