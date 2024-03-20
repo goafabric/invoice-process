fc058194c4f785f57fe0317db5594ce1c11676a1
Andreas Mautsch <amautsch>
2024-03-20 15:51:39 +0100

    changes

--- src/main/java/org/goafabric/invoice/adapter/access/LockAdapter.java
+++ src/main/java/org/goafabric/invoice/adapter/access/LockAdapter.java
@@ -1,12 +1,13 @@
 package org.goafabric.invoice.adapter.access;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.goafabric.invoice.adapter.access.dto.Lock;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.service.annotation.GetExchange;
 
-//@Retry(name = "lockAdapter")
+@Retry(name = "lockAdapter")
 @CircuitBreaker(name = "lockAdapter")
 public interface LockAdapter {
     @GetExchange("locks/acquireLockByKey")
--- src/main/java/org/goafabric/invoice/adapter/access/UserAdapter.java
+++ src/main/java/org/goafabric/invoice/adapter/access/UserAdapter.java
@@ -1,18 +1,18 @@
 package org.goafabric.invoice.adapter.access;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.goafabric.invoice.adapter.access.dto.PermissionCategory;
 import org.goafabric.invoice.adapter.access.dto.PermissionType;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
-//@Retry(name = "userAdapter")
+@Retry(name = "userAdapter")
 @CircuitBreaker(name = "userAdapter")
 @CacheConfig(cacheNames = "userAdapter")
 public interface UserAdapter {
-    @Cacheable
+    //@Cacheable
     @GetExchange("users/hasPermission") //we are using this rather costly method here, to avoid copying to much dto structures for users, roles etc
     Boolean hasPermission(@RequestParam("name") String name, @RequestParam("category") PermissionCategory category, @RequestParam("type") PermissionType type);
 }
