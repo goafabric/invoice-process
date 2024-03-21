0d59265893af430abdfa312a637dfb04600b9568
Andreas Mautsch <amautsch>
2024-03-21 14:24:49 +0100

    build added

--- src/main/java/org/goafabric/invoice/adapter/access/UserAdapter.java
+++ src/main/java/org/goafabric/invoice/adapter/access/UserAdapter.java
@@ -5,6 +5,7 @@ import io.github.resilience4j.retry.annotation.Retry;
 import org.goafabric.invoice.adapter.access.dto.PermissionCategory;
 import org.goafabric.invoice.adapter.access.dto.PermissionType;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
@@ -12,7 +13,7 @@ import org.springframework.web.service.annotation.GetExchange;
 @CircuitBreaker(name = "userAdapter")
 @CacheConfig(cacheNames = "userAdapter")
 public interface UserAdapter {
-    //@Cacheable
+    @Cacheable
     @GetExchange("users/hasPermission") //we are using this rather costly method here, to avoid copying to much dto structures for users, roles etc
     Boolean hasPermission(@RequestParam("name") String name, @RequestParam("category") PermissionCategory category, @RequestParam("type") PermissionType type);
 }
