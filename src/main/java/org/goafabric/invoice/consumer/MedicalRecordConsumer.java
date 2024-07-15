37a0c05aa02354ec0c81dd32b8291dab8352909d
Andreas Mautsch <amautsch>
2024-07-15 20:51:26 +0200

    kafka added

--- src/main/java/org/goafabric/invoice/consumer/MedicalRecordConsumer.java
+++ src/main/java/org/goafabric/invoice/consumer/MedicalRecordConsumer.java
@@ -1,7 +1,7 @@
 package org.goafabric.invoice.consumer;
 
 import org.goafabric.event.EventData;
-import org.goafabric.invoice.process.adapter.patient.dto.Patient;
+import org.goafabric.invoice.process.adapter.patient.dto.MedicalRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,19 +12,19 @@ import org.springframework.stereotype.Component;
 import static org.goafabric.invoice.consumer.ConsumerUtil.withTenantInfos;
 
 @Component
-public class PatientConsumer {
+public class MedicalRecordConsumer {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    static final String CONSUMER_NAME = "Patient";
+    static final String CONSUMER_NAME = "MedicalRecord";
 
-    @KafkaListener(groupId = CONSUMER_NAME, topics = "patient")
+    @KafkaListener(groupId = CONSUMER_NAME, topics = "medicalrecord")
     public void process(@Header(KafkaHeaders.RECEIVED_TOPIC) String topic, EventData eventData) {
         withTenantInfos(() -> process(eventData));
     }
 
     private void process(EventData eventData) {
-        var patient = (Patient) eventData.payload();
-        log.info(patient.toString());
+        var medicalRecord = (MedicalRecord) eventData.payload();
+        log.info(medicalRecord.toString());
         //todo: db insert
     }
 
