cb0a6210f55c7ab1255d6363aa18a2399e93063e
Andreas Mautsch <amautsch>
2024-12-31 15:24:02 +0100

    upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,20 @@ val version: String by project
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 val dockerRegistry = "goafabric"
-val nativeBuilder = "paketobuildpacks/java-native-image:9.8.0"
-val baseImage = "ibm-semeru-runtimes:open-21.0.3_9-jre-focal@sha256:5cb19afa9ee0daeecb7c31be8253fecbbf6b5f6dcfb06883c41f045cb893bcec"
+val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14fd1d4660f9dc777b1ad3630f847b8e3dc371203bcb857a5e74d6c39" //"ibm-semeru-runtimes:open-23_37-jre-focal@sha256:04534a98d0e521948b7525c665f9f8871aba56155de9e70d23b14c905a28a052"
 
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("org.graalvm.buildtools.native") version "0.10.2"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.graalvm.buildtools.native") version "0.10.4"
 
-	id("com.google.cloud.tools.jib") version "3.4.3"
-	id("net.researchgate.release") version "3.0.2"
-	id("org.sonarqube") version "5.0.0.4638"
+	id("com.google.cloud.tools.jib") version "3.4.4"
+	id("net.researchgate.release") version "3.1.0"
+	id("org.sonarqube") version "6.0.1.5171"
+
+	id("org.cyclonedx.bom") version "1.10.0"
 }
 
 repositories {
@@ -87,8 +88,6 @@ jib {
 tasks.register("dockerImageNative") { group = "build"; dependsOn("bootBuildImage") }
 tasks.named<BootBuildImage>("bootBuildImage") {
 	val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
-	builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny")
-	buildpacks.add(nativeBuilder)
 	imageName.set(nativeImageName)
 	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "21", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx5000m -march=compatibility"))
 	doLast {
