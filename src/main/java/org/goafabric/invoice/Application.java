f600b66914249da2e646cd861fe50bf5d37ab367
Andreas Mautsch <amautsch>
2025-11-22 10:38:19 +0100

    spring boot upgrade

--- src/main/java/org/goafabric/invoice/Application.java
+++ src/main/java/org/goafabric/invoice/Application.java
@@ -17,13 +17,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class Application {
 
-    public static void main(String[] args){
+    static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
     public CommandLineRunner init(ApplicationContext context, @Value("${process.autostart:false}") boolean processAutoStart) {
-        return args -> {
+        return _ -> {
             if (processAutoStart) {
                 context.getBean(InvoiceProcess.class).run().get();
                 SpringApplication.exit(context, () -> 0);
