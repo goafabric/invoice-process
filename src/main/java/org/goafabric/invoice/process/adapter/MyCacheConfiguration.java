d556081dcc4a50ee69683deb4900862ee04c368f
Andreas Mautsch <amautsch>
2025-11-27 08:51:48 +0100

    redis fuck

--- src/main/java/org/goafabric/invoice/process/adapter/MyCacheConfiguration.java
+++ src/main/java/org/goafabric/invoice/process/adapter/MyCacheConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
@@ -46,7 +46,7 @@ public class MyCacheConfiguration implements CachingConfigurer {
         return org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(cacheExpiry))
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())); //do NOT dare to upgrade this to new Jackson3 Serizalizer or everything will go to hell !
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJacksonJsonRedisSerializer.builder().enableUnsafeDefaultTyping().build()));
     }
 
     @Bean
