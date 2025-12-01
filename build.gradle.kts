1b450363c79ace684a6bebbbb17ef55f99c213d8
Andreas Mautsch <amautsch>
2025-12-01 08:54:21 +0100

    chang

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,4 +118,4 @@ openApi {
 	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
 
-sonar { properties { property("sonar.coverage.exclusions", "**/ApplicationBaseRuntimeHints.java"); property("sonar.cpd.exclusions", "**/ApplicationBaseRuntimeHints.java") } }
+sonar { properties { property("sonar.exclusions", "**/ApplicationBaseRuntimeHints.java") } }
