fd03ffe92fc6ad7c7255a1b7202e90b0b25f00ce
Andreas Mautsch <amautsch@gmx.de>
2026-04-30 14:14:44 +0000

    Update plugin org.sonarqube to v7.3.0.8198

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 	id("com.google.cloud.tools.jib") version "3.5.3"
 	id("net.researchgate.release") version "3.1.0"
-	id("org.sonarqube") version "7.2.3.7755"
+	id("org.sonarqube") version "7.3.0.8198"
 
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
